01c697ea29fdb9cb503f0547e7d309322b6c5796
HEATHCLIFF <mathias.pele@gmail.com>
2024-04-12 16:35:31 +0200

    creation route utilisateur

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AnnonceController;
 use App\Http\Controllers\MessageController;
+use App\Http\Controllers\utilisateurController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/annonces',[AnnonceController::class, 'getAll']);
@@ -12,6 +13,10 @@ Route::get('/annonces/{annonce}',[AnnonceController::class, 'getAnnonceByID']);
 Route::delete('/annonces/delete/{annonce}',[AnnonceController::class, 'deleteById']);
 Route::patch('/annonces/update/{annonce}',[AnnonceController::class, 'updateById']);
 
-
-Route::post('/message/create', [MessageController::class, 'addMessage']);
 Route::get("/message", [MessageController::class, 'getAll']);
+Route::post('/message/create', [MessageController::class, 'addMessage']);
+
+Route::get("/utilisateur",[MessageController::class, 'getAll']);
+
+
+
